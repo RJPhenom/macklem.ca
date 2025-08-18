14510386e0418ae7e716efd8d16c4bf547ea5b5b
RJ <103343995+RJPhenom@users.noreply.github.com>
2025-08-17 20:53:48 -0400

    fix base route

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/",
+	base: "/macklem.ca",
 	plugins: [react(), tailwindcss()],
 });
